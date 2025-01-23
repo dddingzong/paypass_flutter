fe4a2f7bf55d0140068220db057e42f54392e510
USER <cji513@naver.com>
2025-01-23 22:08:08 +0900

    feat(get_stations_service): 데이터 유형 변경 및 한글 깨짐 버그 해결결

--- lib/utils/get_stations_service.dart
+++ lib/utils/get_stations_service.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:paypass/variables/globals.dart';
@@ -12,17 +11,19 @@ class GetStationsService {
     final url = Uri.parse('http://${Constants.ip}/getStations');
     try {
       final response = await http.get(url);
-      final List<dynamic> jsonData = json.decode(response.body);
+      final List<dynamic> jsonData =
+          json.decode(utf8.decode(response.bodyBytes));
 
       stations = jsonData
           .map((station) => {
                 'name': station['name'],
                 'stationNumber': station['stationNumber'],
-                'center': LatLng(station['latitude'], station['longitude']),
+                'latitude': station['latitude'],
+                'longitude': station['longitude'],
               })
           .toList();
 
-      print("정류장 데이터 로드 성공: $stations");
+      print("정류장 데이터 로드 성공");
     } catch (error) {
       print("정류장 데이터 로드 중 오류 발생: $error");
     }
