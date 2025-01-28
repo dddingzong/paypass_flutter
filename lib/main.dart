cad2aab2b01c1f069ad869c97425f37603a0fba7
USER <cji513@naver.com>
2025-01-28 15:31:50 +0900

    feat(main): loadingScreen 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:paypass/screens/login_screen.dart';
+import 'package:paypass/screens/loading_screen.dart';
 
 // main: 애플리케이션의 진입점
 // runApp: Flutter 위젯 트리를 시작하는 함수, 여기서는 MyApp을 루트 위젯으로 설정
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PayPass',
-      home: LoginScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadingScreen(), // 앱 시작 시 로딩 화면 표시
     );
   }
 }
