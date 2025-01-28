1787a900f7158c9153a81359d84228a9910f4bb8
USER <cji513@naver.com>
2025-01-28 15:34:41 +0900

    feat(new_user_screen): UI 개선

--- lib/screens/new_user_screen.dart
+++ lib/screens/new_user_screen.dart
@@ -1,15 +1,19 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:paypass/utils/logger.dart';
+
 import 'package:paypass/variables/constants.dart';
 import 'package:paypass/variables/globals.dart';
-
 import 'map_screen.dart';
 
 class NewUserScreen extends StatelessWidget {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController birthController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
+
+  NewUserScreen({super.key});
 
   bool _isValidDate(String date) {
     final regex = RegExp(r'^\d{4}-\d{2}-\d{2}$');
@@ -27,17 +31,17 @@ class NewUserScreen extends StatelessWidget {
     final phone = phoneController.text.trim();
 
     if (name.isEmpty || birth.isEmpty || phone.isEmpty) {
-      print('모든 필드를 입력하세요.');
+      logger.i('모든 필드를 입력하세요.');
       return;
     }
 
     if (!_isValidDate(birth)) {
-      print('생년월일 형식이 잘못되었습니다. (YYYY-MM-DD)');
+      logger.i('생년월일 형식이 잘못되었습니다. (YYYY-MM-DD)');
       return;
     }
 
     if (!_isValidPhone(phone)) {
-      print('전화번호 형식이 잘못되었습니다.');
+      logger.i('전화번호 형식이 잘못되었습니다.');
       return;
     }
 
@@ -53,19 +57,34 @@ class NewUserScreen extends StatelessWidget {
     );
 
     if (response.statusCode == 200) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => MapScreen()),
-      );
+      if (context.mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MapScreen()),
+        );
+      }
     } else {
-      print('회원가입 실패: ${response.statusCode}');
+      logger.e('회원가입 실패: ${response.statusCode}');
     } // response.statusCode != 200
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("회원 정보 입력")),
+      appBar: AppBar(
+        flexibleSpace: Stack(
+          children: [
+            Positioned(
+              top: 40, // 화면 상단에서 40px 아래
+              left: 20, // 화면 왼쪽에서 20px 오른쪽
+              child: Image.asset(
+                'assets/logo.png', // 로고 이미지 경로
+                width: 40, // 로고 너비
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
