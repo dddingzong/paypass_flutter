f528da19753697c43e559d0673e6bbe32b3f7d75
USER <cji513@naver.com>
2025-01-28 15:36:25 +0900

    chore(geofence_service): log 기능 도입

--- lib/utils/geofence_service.dart
+++ lib/utils/geofence_service.dart
@@ -1,27 +1,48 @@
+import 'dart:convert';
+
 import 'package:geofence_foreground_service/constants/geofence_event_type.dart';
 import 'package:geofence_foreground_service/exports.dart';
 import 'package:geofence_foreground_service/geofence_foreground_service.dart';
 import 'package:geofence_foreground_service/models/zone.dart';
-import 'package:paypass/variables/globals.dart';
 
 import 'package:latlng/latlng.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:paypass/utils/logger.dart';
+import 'package:paypass/variables/constants.dart';
+import 'package:paypass/variables/globals.dart';
 
 @pragma('vm:entry-point')
 void callbackDispatcher() async {
   GeofenceForegroundService().handleTrigger(
     backgroundTriggerHandler: (zoneID, triggerType) {
       if (triggerType == GeofenceEventType.enter) {
-        print('Entered geofence: $zoneID');
+        logger.i('Entered geofence: $zoneID'); // 데이터 저장 로직으로 변경
+        // userFenceIn(zoneID);
       } else if (triggerType == GeofenceEventType.exit) {
-        print('Exited geofence: $zoneID');
+        logger.i('Exited geofence: $zoneID'); // 데이터 수정 로직으로 변경
       } else if (triggerType == GeofenceEventType.dwell) {
-        print('Dwelled geofence: $zoneID');
+        logger.i('Dwelled geofence: $zoneID');
       } else {
-        print('Unknown type in geofence');
+        logger.i('Unknown type in geofence');
       }
       return Future.value(true);
     },
   );
+}
+
+Future<void> userFenceIn(zoneID) async {
+  //zoneID의 출력값은 stationNumber값이다.
+  final response = await http.post(
+    Uri.parse('http://${Constants.ip}/userFenceIn'),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({'zoneId': zoneID}), // stationNumber 값만 넘기기
+  );
+
+  if (response.statusCode == 200) {
+    // ignore: unused_local_variable
+    final jsonData = json.decode(utf8.decode(response.bodyBytes));
+  }
 }
 
 void setupGeofenceService() async {
